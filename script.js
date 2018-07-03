271b8e88aed01755b0ecd079bc574e701ab4724c
Melanie Rogan <melanie.rogan@ft.com>
2018-07-03 16:53:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-There is where the backend sits where the topics get randomised
+
+document.addEventListener("DOMContentLoaded",function(){
+    
+
+  const quotesPool = [["Migrate from Travis to CircleCI 2.0"],            
+  ["Test coverage"],  
+  ["Migrate our config vars to Vault"],  
+  ["Security fixes (npm audit, whitesource, github warnings)"],  
+  ["Integrate the IP flags client into apps"],   
+  ["Linting"]];
+
+    let oldQuoteIndex;
+
+    function generateNumber(dataPool) {
+      return Math.floor(Math.random() * dataPool.length);
+    }
+  
+    function generateNewQuote() {
+      let index = generateNumber(quotesPool);
+
+      while (index === oldQuoteIndex) {
+        index = generateNumber(quotesPool);
+      }
+      let newQuote = quotesPool[index];
+      let quote = document.getElementById("quote");
+
+      quote.innerHTML = newQuote[0];
+
+
+      oldQuoteIndex = index;
+    }
+  
+    function onQuoteButtonClick() {
+      let randomQuoteButton = document.querySelector("#random-quote");
+      randomQuoteButton.addEventListener("click", function(){
+        generateNewQuote();
+      });
+    }
+  
+    onQuoteButtonClick();
+  
+    window.onload = function () {
+      generateNewQuote();
+
+    };
+    
+  });
+  
