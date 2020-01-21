eeea04f1d65db3a204dc08a78cbe8fe883ba15c8
Melanie Rogan <melanie.rogan@ft.com>
2020-01-21 12:03:17 +0000

    making the url changeable

--- server/app.js
+++ server/app.js
@@ -2,20 +2,18 @@ const express = require('express');
 const exphbs = require('express-handlebars');
 const swarmThings = require('../server/controllers/get-swarm-list-controller');
 const createApp = () => {
-    const app = express();
+	const app = express();
 
-    const handlebarsInstance = exphbs.create({
-        extname: '.html',
-        defaultLayout: 'main'
-    });
+	const handlebarsInstance = exphbs.create({
+		extname: '.html',
+		defaultLayout: 'main',
+	});
 
-
-    app.engine('html', handlebarsInstance.engine);
-    app.use(express.static('public'))
-    app.set('view engine', '.html');
-    app.get('/', swarmThings);
-return app;
-
+	app.engine('html', handlebarsInstance.engine);
+	app.use(express.static('public'));
+	app.set('view engine', '.html');
+	app.get('/', swarmThings);
+	return app;
 };
 const port = process.env.PORT || 3000;
 const app = createApp();
--- server/controllers/get-swarm-list-controller.js
+++ server/controllers/get-swarm-list-controller.js
@@ -1,11 +1,12 @@
 const fetch = require('node-fetch');
 
 const swarmThings = async (req, res) => {
-	
 	try {
-		const response = await fetch('https://bertha.ig.ft.com/view/publish/gss/1thHNlUAXOtYtm14k0gZpfe19DPGdad-Lr7XvLdIgXuc/Sheet1', { method: 'GET', headers: {
-			'Content-type': 'application/json',
-		},
+		const response = await fetch(`${process.env.SWARM_API_URL}`, {
+			method: 'GET',
+			headers: {
+				'Content-type': 'application/json',
+			},
 		});
 
 		if (response.status === 404) {
@@ -15,13 +16,13 @@ const swarmThings = async (req, res) => {
 		if (response.status >= 500) {
 			throw new Error('internal error');
 		}
-		
-		if (response.status === 200) {
-			const json = await response.json(); 
 
-			const debug = JSON.stringify(json)
-			res.render('home', { themes: json, debug: debug })        
-		} 
+		if (response.status === 200) {
+			const json = await response.json();
+
+			const debug = JSON.stringify(json);
+			res.render('home', { themes: json, debug: debug });
+		}
 	} catch (error) {
 		// eslint-disable-next-line no-console
 		console.log(error);
